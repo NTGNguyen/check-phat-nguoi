f42286a4d8a51c49a34af15309e75d1c7a90d3fa
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-23 13:26:28 +0700

    feat: add get_data function to fetch data for mutiple plates

--- src/check_phat_nguoi/modules/get_data.py
+++ src/check_phat_nguoi/modules/get_data.py
@@ -1,6 +1,6 @@
-"""Get datas"""
+"""Get data"""
 
-import threading
+from threading import Thread
 from typing import Dict
 
 import requests
@@ -19,31 +19,48 @@ class GetData:
         Args:
             plate_infos: List of PlateInfo
         """
-        self._plate_infos = plate_infos
+        self._plate_infos: list[PlateInfo] = plate_infos
+        self.data_dict: Dict[str, None | Dict] = {}
 
-    def _get_data(self) -> None | Dict:
+    def _get_data(self, plate: str) -> None:
         """Get data with a single object
 
+        Args:
+            plate: plate's information
+
         Returns:
-            None | Dict: _description_
+            None | Dict: a dict
         """
-        # TODO: Iterate từng _plate_infos lấy biển plate nha Nguyễn
-        payload: dict[str, str] = {"bienso": ""}
+        payload: dict[str, str] = {"bienso": f"{plate}"}
         try:
             response: Response = requests.post(url=URL, json=payload)
             response.raise_for_status()
             response_data: Dict = response.json()
             if response_data.get("data") is None:
-                return None
+                self.data_dict[plate] = None
             else:
-                return response_data
-        except:
+                self.data_dict[plate] = response_data
+        except Exception:
+            self._data_dict[plate] = None
+
+    def get_data(self) -> Dict[str, None | Dict] | None:
+        """Get data
+
+        Returns:
+            Dict: A dictionary mapping plate to response data.
+        """
+
+        threads: list[Thread] = []
+        try:
+            for plate_info in self._plate_infos:
+                thread = Thread(target=self._get_data,
+                                args=(plate_info.plate,))
+                threads.append(thread)
+                thread.start()
+
+            for thread in threads:
+                thread.join()
+
+            return self.data_dict
+        except Exception:
             return None
-
-
-class _GetDataThread(threading.Thread):
-    pass
-
-
-class GetDataMultiThread:
-    pass
