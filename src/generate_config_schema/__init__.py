da8752b4ce2e21cee00ec2b81d8621d809fc2e20
KevinNitroG <trannguyenthaibinh46@gmail.com>
2024-12-24 01:27:24 +0700

    fix: indent for config schema

--- src/generate_config_schema/__init__.py
+++ src/generate_config_schema/__init__.py
@@ -8,4 +8,4 @@ from check_phat_nguoi.models.config import Config
 def main():
     adapter = TypeAdapter(Config)
     with open("schemas/config.json", "w", encoding="utf8") as file:
-        file.write(dumps(adapter.json_schema()))
+        file.write(dumps(adapter.json_schema(), indent=2))
