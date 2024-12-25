4624b8c23f5066c6a3eab494ea1948768a8c615d
NTGNguyen <23521049@gm.uit.edu.vn>
2024-12-25 13:00:44 +0700

    refactor: remove redundant function as its logic is handled in another method

--- src/check_phat_nguoi/context/plates_context.py
+++ src/check_phat_nguoi/context/plates_context.py
@@ -1,16 +1,9 @@
-from typing import Dict
-
 from check_phat_nguoi.models.context import PlatesContextModel
 from check_phat_nguoi.utils.singleton import Singleton
 
 
 class PlatesContext(Singleton, PlatesContextModel):
-    def get_context(self, data_dict: Dict[str, None | Dict], owner: str = "Unknown"):
-        for plate, plate_violation_dict in data_dict.items():
-            plate_info_dict = {"plate": plate, "owner": owner}
-            plate_info_model = PlateInfoContextModel(**plate_info_dict)
-            plate_info_model.get_plate_info(plate, plate_violation_dict)
-            self.plates.append(plate_info_model)
+    pass
 
 
 plates_context: PlatesContext = PlatesContext()
