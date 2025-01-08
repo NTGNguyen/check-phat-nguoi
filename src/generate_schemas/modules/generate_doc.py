acdc38145052b83dbc6be420532cb485c262f653
NTGNguyen <23521049@gm.uit.edu.vn>
2025-01-08 19:20:49 +0700

    fix: correct file path after restructure

--- src/generate_schemas/modules/generate_doc.py
+++ src/generate_schemas/modules/generate_doc.py
@@ -1,6 +1,8 @@
 from json_schema_for_humans.generate import generate_from_filename
 from json_schema_for_humans.generation_configuration import GenerationConfiguration
 
+from ..utils.constants import CONFIG_SCHEMA_PATH, GENERATE_SCHEMAS_WEBSITES_PATH
+
 
 def generate_doc():
     config: GenerationConfiguration = GenerationConfiguration(
@@ -8,7 +10,7 @@ def generate_doc():
     )
 
     generate_from_filename(
-        "schemas/config.json", "sites/schemas/schema_doc.html", config=config
+        CONFIG_SCHEMA_PATH, GENERATE_SCHEMAS_WEBSITES_PATH, config=config
     )
 
 
