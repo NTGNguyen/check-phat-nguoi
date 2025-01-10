dd1aa79918154a4de8c5f3ba15613991277ac9bb
KevinNitroG <trannguyenthaibinh46@gmail.com>
2025-01-10 11:41:23 +0700

    fix: schema config

--- src/check_phat_nguoi/config/models/plate_info.py
+++ src/check_phat_nguoi/config/models/plate_info.py
@@ -22,15 +22,15 @@ class PlateInfo(BaseModel):
         title="Biển số",
         examples=["60A64685", "98-A-56604", "30-F88251", "59XB-00000"],
     )
+    type: VehicleStrType | VehicleTypeEnum = Field(
+        description="Loại phương tiện để gửi request cũng như lọc loại phương tiện đối với các API không lọc loại phương tiện sẵn",
+        title="Loại phương tiện",
+    )
     owner: str | None = Field(
         description="Ghi chú chủ sở hữu (phù hợp khi dùng nhắc ai đó với lựa chọn notifications)",
         title="Ghi chú chủ sở hữu",
         examples=["@kevinnitro", "dad"],
         default=None,
-    )
-    type: VehicleStrType | VehicleTypeEnum = Field(
-        description="Loại phương tiện để gửi request cũng như lọc loại phương tiện đối với các API không lọc loại phương tiện sẵn",
-        title="Loại phương tiện",
     )
     api: ApiEnum | None = Field(
         description="Sử dụng API từ trang web nào (để trống sẽ sử dụng API define ở scope ngoài)",
