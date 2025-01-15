a95fe8a53424bdaaf35131fdf41d4f3b6ddb7457
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-01-15 16:25:30 +0700

    refactor: declare str dunder method in suitable models

--- src/check_phat_nguoi/context/plates/models/plate_detail.py
+++ src/check_phat_nguoi/context/plates/models/plate_detail.py
@@ -42,60 +42,22 @@ class PlateDetail(BaseModel):
         return False
 
     # TODO: Handle show details later when main updates that option
+    @override
     def __str__(self) -> str:
-        def create_violation_str(violation: ViolationDetail, index: int) -> str:
-            violation_str: str = (
-                f"Lỗi vi phạm thứ {index}:"
-                + (f"\nMàu biển: {violation.color}" if violation.color else "")
-                + (f"\nThời điểm vi phạm: {violation.date}" if violation.date else "")
-                + (
-                    f"\nVị trí vi phạm: {violation.location}"
-                    if violation.location
-                    else ""
-                )
-                + (
-                    f"\nHành vi vi phạm: {violation.violation}"
-                    if violation.violation
-                    else ""
-                )
-                + (
-                    f"\nTrạng thái: {'Chưa xử phạt' if not violation.status else 'Đã xử phạt'}"
-                    if violation.status is not None
-                    else ""
-                )
-                + (
-                    f"\nĐơn vị phát hiện vi phạm: {violation.enforcement_unit}"
-                    if violation.enforcement_unit
-                    else ""
-                )
-            )
-            resolution_offices: str | None = (
-                "\n"
-                + "Nơi giải quyết vụ việc:"
-                + "\n"
-                + "\n".join(
-                    resolution_office_detail.strip()
-                    for resolution_office_detail in violation.resolution_offices
-                )
-                if violation.resolution_offices
-                else None
-            )
-            return violation_str + (resolution_offices if resolution_offices else "")
-
         plate_detail: str = f"Biển số: {self.plate}" + (
             f"\nChủ sở hữu: {self.owner}" if self.owner else ""
         )
-
         if self.violations:
             return (
                 plate_detail
                 + "\n"
                 + "\n".join(
-                    create_violation_str(violation, index)
-                    for index, violation in enumerate(self.violations, start=1)
+                    f"Lỗi vi phạm #{order}:\n" + str(violation)
+                    for order, violation in enumerate(self.violations, start=1)
                 )
             )
-        return plate_detail
+        else:
+            return plate_detail
 
 
 __all__ = ["PlateDetail"]
--- src/check_phat_nguoi/context/plates/models/violation_detail.py
+++ src/check_phat_nguoi/context/plates/models/violation_detail.py
@@ -5,7 +5,7 @@ from typing import override
 
 from pydantic import BaseModel, ConfigDict, Field
 
-from check_phat_nguoi.types import VehicleTypeEnum
+from check_phat_nguoi.types import VehicleTypeEnum, get_vehicle_str_vie
 
 
 # NOTE: This class is used to store the get data's reponse. So it has None type in each field
@@ -52,5 +52,33 @@ class ViolationDetail(BaseModel):
     )
 
     @override
+    def __str__(self) -> str:
+        return (
+            (f"Biển: {self.plate}" if self.plate else "")
+            + (f"\nMàu biển: {self.color}" if self.color else "")
+            + (f"\nLoại xe: {get_vehicle_str_vie(self.type)}" if self.type else "")
+            + (f"\nThời điểm vi phạm: {self.date}" if self.date else "")
+            + (f"\nVị trí vi phạm: {self.location}" if self.location else "")
+            + (f"\nHành vi vi phạm: {self.violation}" if self.violation else "")
+            + (
+                f"\nTrạng thái: {'Đã xử phạt' if self.status else 'Chưa xử phạt'}"
+                if self.status
+                else ""
+            )
+            + (
+                f"\nĐơn vị phát hiện vi phạm: {self.enforcement_unit}"
+                if self.enforcement_unit
+                else ""
+            )
+            + (
+                "\n" + "\n".join(self.resolution_offices)
+                if self.resolution_offices
+                else ""
+            )
+        ).strip()
+
+    @override
     def __hash__(self):
-        return hash(self.date) + hash(self.location)
+        return (
+            hash(self.plate) + hash(self.color) + hash(self.date) + hash(self.location)
+        )
