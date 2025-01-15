e48a9dd84e482cad550a67b863398a2fe324894c
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-01-16 00:17:37 +0700

    chore: remove unused exceptions

--- src/check_phat_nguoi/get_data/engines/exceptions.py
+++
@@ -1,9 +0,0 @@
-class ParsingDataWhileGettingDataException(Exception):
-    def __init__(
-        self, message: str = "Error when parsing data while getting data"
-    ) -> None:
-        super().__init__(message)
-        self.message: str = message
-
-    def __str__(self) -> str:
-        return self.message
