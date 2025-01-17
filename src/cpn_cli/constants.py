65bcabd11c4c1bf6709b499a0620797acfd8984d
Kevin Nitro <trannguyenthaibinh46@gmail.com>
2025-01-18 01:38:10 +0700

    chore: more details log

--- src/cpn_cli/constants.py
+++ src/cpn_cli/constants.py
@@ -7,5 +7,5 @@ CONFIG_PATHS: tuple[LiteralString, ...] = (
 )
 SIMPLE_LOG_MESSAGE: LiteralString = "[%(levelname)s]: %(message)s"
 DETAIL_LOG_MESSAGE: LiteralString = (
-    "%(asctime)s [%(levelname)s] - %(message)s (%(filename)s:%(lineno)d - %(module)s)"
+    "%(asctime)s [%(levelname)s] - %(message)s (%(filename)s:%(lineno)d - %(pathname)s)"
 )
